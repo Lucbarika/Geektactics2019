67fdb0249de7d9752ec0d53c711df765ff80db27
alex george <alexandreantoinegeorge@gmail.com>
2019-01-04 19:44:02 -0500

    tutor page added

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,7 +2,10 @@ class DashboardsController < ApplicationController
   def tutor_dashboard
     @tutor = current_user
   end
-
+  def tutor_course
+    @tutor_tools = current_user.tools
+    @tutor_courses = @tutor_tools.courses
+  end
   def student_dashboard
     @student = current_user
   end
