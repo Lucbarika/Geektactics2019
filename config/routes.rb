ca89cb42243dcc7893277cb7a0802292528c1c7c
alex george <alexandreantoinegeorge@gmail.com>
2019-01-06 19:59:41 -0500

    tool#show coded

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get '/contact', to: 'pages#contact', as: 'contact'
 
   resources :courses, only: [:show]
+  resources :tools, only: [:show]
+
   get 'tools/tools_course_index', to: "tools#tools_course_index", as: 'tools_course_index'
   get 'users/tutor_dashboards', to: 'dashboards#tutor_dashboard', as: 'tutor_dashboard'
   get 'users/student_dashboards', to: 'dashboards#student_dashboard', as: 'student_dashboard'
