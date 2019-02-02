f1ad5c0aec9adfbc206bd5edcc296d23c7eecf11
camille villard <hello@camillevillard.com>
2019-02-02 14:22:38 -0500

    basic font scheme

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,7 +19,7 @@ User.destroy_all
 # Add new basic elements
 puts "Seed generation"
 categories = ["math", "physics", "finance", "economy", "informatic", "literature", "language", "phylosophy", "sociology"]
-lorem_ipsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+lorem_ipsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore."
 
 # Users generation
 # 3 student
