8bd1c1ced8b033890171fae428049aade85f4b8a
alex george <alexandreantoinegeorge@gmail.com>
2019-02-06 19:30:52 -0500

    seed generated

--- db/seeds.rb
+++ db/seeds.rb
@@ -119,7 +119,7 @@ students = [user_test_mvp, student1, student2, student3, student4]
 # Course generation
 math_derivation = Course.new(
   title: "Derivation",
-  description: "lorem_ipsum",
+  description: lorem_ipsum,
   category: "Maths",
   code: "math0001"
   )
@@ -167,35 +167,179 @@ math_derivation.save!
           )
   tool_math_derivation4.user = teachers.sample
   tool_math_derivation4.course = math_derivation
-  tool_math_derivation3.save!
+  tool_math_derivation4.save!
 
 math_matrix = Course.new(
   title: "Matrix",
-  description: "lorem_ipsum",
+  description: lorem_ipsum,
   category: "Maths",
   code: "math0002"
   )
 math_matrix.save!
 
+    tool_math_matrix1 = Tool.new(
+        name: "Matrix Basic",
+        description: lorem_ipsum,
+        price: (1..10).to_a.sample * 100,
+        pdf_url: "pdf_url_test",
+        video_url: "https://www.youtube.com/embed/fKFbnhcNnjE?list=PL6AuTaocqrRP_tnkjSdjpFhnQeTjoxkSx"
+          )
+  tool_math_matrix1.user = teachers.sample
+  tool_math_matrix1.course = math_matrix
+  tool_math_matrix1.save!
+
+  tool_math_matrix2 = Tool.new(
+        name: "Matrix level 1",
+        description: lorem_ipsum,
+        price: (1..10).to_a.sample * 100,
+        pdf_url: "pdf_url_test",
+        video_url: "https://www.youtube.com/embed/fKFbnhcNnjE?list=PL6AuTaocqrRP_tnkjSdjpFhnQeTjoxkSx"
+          )
+  tool_math_matrix2.user = teachers.sample
+  tool_math_matrix2.course = math_matrix
+  tool_math_matrix2.save!
+
+    tool_math_matrix3 = Tool.new(
+        name: "Matrix level2",
+        description: lorem_ipsum,
+        price: (1..10).to_a.sample * 100,
+        pdf_url: "pdf_url_test",
+        video_url: "https://www.youtube.com/embed/fKFbnhcNnjE?list=PL6AuTaocqrRP_tnkjSdjpFhnQeTjoxkSx"
+          )
+  tool_math_matrix3.user = teachers.sample
+  tool_math_matrix3.course = math_matrix
+  tool_math_matrix3.save!
+
+  tool_math_matrix4 = Tool.new(
+        name: "Matrix Advanced",
+        description: lorem_ipsum,
+        price: (1..10).to_a.sample * 100,
+        pdf_url: "pdf_url_test",
+        video_url: "https://www.youtube.com/embed/fKFbnhcNnjE?list=PL6AuTaocqrRP_tnkjSdjpFhnQeTjoxkSx"
+          )
+  tool_math_matrix4.user = teachers.sample
+  tool_math_matrix4.course = math_matrix
+  tool_math_matrix4.save!
+
+
 phy_light = Course.new(
   title: "Lighting theory",
-  description: "lorem_ipsum",
+  description: lorem_ipsum,
   category: "Physiques",
   code: "phy0001"
   )
 phy_light.save!
 
+  tool_phy_light1 = Tool.new(
+    name: "Lighting basics",
+    description: lorem_ipsum,
+    price: (1..10).to_a.sample * 100,
+    pdf_url: "pdf_url_test",
+    video_url: "https://www.youtube.com/embed/fKFbnhcNnjE?list=PL6AuTaocqrRP_tnkjSdjpFhnQeTjoxkSx"
+      )
+  tool_phy_light1.user = teachers.sample
+  tool_phy_light1.course = phy_light
+  tool_phy_light1.save!
+
+  tool_phy_light2 = Tool.new(
+    name: "Lighting level 1",
+    description: lorem_ipsum,
+    price: (1..10).to_a.sample * 100,
+    pdf_url: "pdf_url_test",
+    video_url: "https://www.youtube.com/embed/fKFbnhcNnjE?list=PL6AuTaocqrRP_tnkjSdjpFhnQeTjoxkSx"
+      )
+  tool_phy_light2.user = teachers.sample
+  tool_phy_light2.course = phy_light
+  tool_phy_light2.save!
+
+  tool_phy_light3 = Tool.new(
+    name: "Lighting level2",
+    description: lorem_ipsum,
+    price: (1..10).to_a.sample * 100,
+    pdf_url: "pdf_url_test",
+    video_url: "https://www.youtube.com/embed/fKFbnhcNnjE?list=PL6AuTaocqrRP_tnkjSdjpFhnQeTjoxkSx"
+      )
+  tool_phy_light2.user = teachers.sample
+  tool_phy_light2.course = phy_light
+  tool_phy_light2.save!
+
+    tool_phy_light3 = Tool.new(
+    name: "Lighting level 3",
+    description: lorem_ipsum,
+    price: (1..10).to_a.sample * 100,
+    pdf_url: "pdf_url_test",
+    video_url: "https://www.youtube.com/embed/fKFbnhcNnjE?list=PL6AuTaocqrRP_tnkjSdjpFhnQeTjoxkSx"
+      )
+  tool_phy_light3.user = teachers.sample
+  tool_phy_light3.course = phy_light
+  tool_phy_light3.save!
+
+  tool_phy_light4 = Tool.new(
+    name: "Lighting Advanced",
+    description: lorem_ipsum,
+    price: (1..10).to_a.sample * 100,
+    pdf_url: "pdf_url_test",
+    video_url: "https://www.youtube.com/embed/fKFbnhcNnjE?list=PL6AuTaocqrRP_tnkjSdjpFhnQeTjoxkSx"
+      )
+  tool_phy_light4.user = teachers.sample
+  tool_phy_light4.course = phy_light
+  tool_phy_light4.save!
+
 phy_nuclear = Course.new(
   title: "Nuclear and radioactivity",
-  description: "lorem_ipsum",
+  description: lorem_ipsum,
   category: "Physiques",
   code: "phy0002"
   )
 phy_nuclear.save!
 
+  tool_phy_nuclear1 = Tool.new(
+    name: "Nuclear basics",
+    description: lorem_ipsum,
+    price: (1..10).to_a.sample * 100,
+    pdf_url: "pdf_url_test",
+    video_url: "https://www.youtube.com/embed/fKFbnhcNnjE?list=PL6AuTaocqrRP_tnkjSdjpFhnQeTjoxkSx"
+      )
+  tool_phy_nuclear1.user = teachers.sample
+  tool_phy_nuclear1.course = phy_nuclear
+  tool_phy_nuclear1.save!
+
+    tool_phy_nuclear2 = Tool.new(
+    name: "Nuclear Advanced",
+    description: lorem_ipsum,
+    price: (1..10).to_a.sample * 100,
+    pdf_url: "pdf_url_test",
+    video_url: "https://www.youtube.com/embed/fKFbnhcNnjE?list=PL6AuTaocqrRP_tnkjSdjpFhnQeTjoxkSx"
+      )
+  tool_phy_nuclear2.user = teachers.sample
+  tool_phy_nuclear2.course = phy_nuclear
+  tool_phy_nuclear2.save!
+
+  tool_phy_nuclear3 = Tool.new(
+    name: "Radioactivity basics",
+    description: lorem_ipsum,
+    price: (1..10).to_a.sample * 100,
+    pdf_url: "pdf_url_test",
+    video_url: "https://www.youtube.com/embed/fKFbnhcNnjE?list=PL6AuTaocqrRP_tnkjSdjpFhnQeTjoxkSx"
+      )
+  tool_phy_nuclear3.user = teachers.sample
+  tool_phy_nuclear3.course = phy_nuclear
+  tool_phy_nuclear3.save!
+
+    tool_phy_nuclear4 = Tool.new(
+    name: "Radioactivity Advanced",
+    description: lorem_ipsum,
+    price: (1..10).to_a.sample * 100,
+    pdf_url: "pdf_url_test",
+    video_url: "https://www.youtube.com/embed/fKFbnhcNnjE?list=PL6AuTaocqrRP_tnkjSdjpFhnQeTjoxkSx"
+      )
+  tool_phy_nuclear4.user = teachers.sample
+  tool_phy_nuclear4.course = phy_nuclear
+  tool_phy_nuclear4.save!
+
 french = Course.new(
   title: "French class",
-  description: "lorem_ipsum",
+  description: lorem_ipsum,
   category: "Language",
   code: "lan0001"
   )
@@ -203,7 +347,7 @@ french.save!
 
 chinese = Course.new(
   title: "Chinese class",
-  description: "lorem_ipsum",
+  description: lorem_ipsum,
   category: "Language",
   code: "lan0002"
   )
@@ -211,7 +355,7 @@ chinese.save!
 
 literature = Course.new(
   title: "Literature",
-  description: "lorem_ipsum",
+  description: lorem_ipsum,
   category: "Language",
   code: "lan0003"
   )
@@ -219,7 +363,7 @@ literature.save!
 
 economy_macro = Course.new(
   title: "Macro Economy",
-  description: "lorem_ipsum",
+  description: lorem_ipsum,
   category: "Economy",
   code: "eco0001"
   )
@@ -227,7 +371,7 @@ economy_macro.save!
 
 economy_micro = Course.new(
   title: "Micro Economy",
-  description: "lorem_ipsum",
+  description: lorem_ipsum,
   category: "Economy",
   code: "eco0002"
   )
@@ -235,7 +379,7 @@ economy_micro.save!
 
 classical_music = Course.new(
   title: "Classical Music Basic",
-  description: "lorem_ipsum",
+  description: lorem_ipsum,
   category: "Music",
   code: "mus0001"
   )
@@ -243,7 +387,7 @@ classical_music.save!
 
 ruby_coding = Course.new(
   title: "Ruby and Rails",
-  description: "lorem_ipsum",
+  description: lorem_ipsum,
   category: "informatique",
   code: "inf0001"
   )
@@ -251,7 +395,7 @@ ruby_coding.save!
 
 python_coding = Course.new(
   title: "Python",
-  description: "lorem_ipsum",
+  description: lorem_ipsum,
   category: "informatique",
   code: "inf0003"
   )
@@ -259,8 +403,21 @@ python_coding.save!
 
 front_end = Course.new(
   title: "Front End",
-  description: "lorem_ipsum",
+  description: lorem_ipsum,
   category: "informatique",
   code: "inf0004"
   )
 front_end.save!
+
+courses = [math_derivation, math_matrix, phy_light, phy_nuclear, french, chinese, literature, economy_macro, economy_micro, classical_music, ruby_coding, python_coding, front_end]
+
+#  COourse feedbacks
+
+15.times do
+  course_feedback = CourseFeedback.new(
+    content: lorem_ipsum
+  )
+  course_feedback.course = courses.sample
+  course_feedback.user = students.sample
+  course_feedback.save!
+end
